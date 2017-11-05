108d4745c170ddd255fc7371f4910b21657f2689
TonyMartinez <tonitonytoni11@gmail.com>
2017-11-05 21:39:16 +0300

    First commit

---
+++ app/src/main/java/com/example/tony/mustapp/view/PosterActivity.java
@@ -0,0 +1,36 @@
+package com.example.tony.mustapp.view;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tony.mustapp.R;
+
+public class PosterActivity extends AppCompatActivity {
+
+    private Fragment fragment = null;
+    private Class fragmentClass = null;
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poster);
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentClass = InfoFragment.class;
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        fragmentManager.beginTransaction().add(R.id.container, fragment).commit();
+
+
+
+    }
+}
